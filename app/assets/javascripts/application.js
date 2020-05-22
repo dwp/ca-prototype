ae4488c4079a1f13df2da132c77ab7fda515c37d
David Todd <me@david.to>
2020-05-22 11:12:57 +0100

    updates to service name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,22 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+	window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
 $(document).ready(function () {
-  window.GOVUKFrontend.initAll()
+	window.GOVUKFrontend.initAll()
+})
+
+// Reload page when user goes back so data can be refreshed
+
+window.addEventListener('pageshow', (event) => {
+	const historyTraversal =
+		event.persisted ||
+		(typeof window.performance != 'undefined' &&
+			window.performance.navigation.type === 2)
+	if (historyTraversal) {
+		// Handle page restore.
+		window.location.reload()
+	}
 })
