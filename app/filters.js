d45481dba90b407e49b002a2f6ab5e732a059386
Nathan Maybrey <nathan.maybrey@engineering.digital.dwp.gov.uk>
2022-09-05 13:59:05 +0100

    Dynamic routing for claim date recommendation

--- app/filters.js
+++ app/filters.js
@@ -1,236 +1,315 @@
 const { date } = require('faker/lib/locales/en')
 const { v4: uuid } = require('uuid')
+const { DateTime } = require('luxon')
 
 const isValidDate = (d) => {
-	return d instanceof Date && !isNaN(d)
+  return d instanceof Date && !isNaN(d)
 }
 
 const isNotThere = (input) => {
-	return !input || input.trim() == '' || input.trim() == 'undefined'
+  return !input || input.trim() == '' || input.trim() == 'undefined'
 }
 
 module.exports = function (env) {
-	/**
-	 * Instantiate object used to store the methods registered as a
-	 * 'filter' (of the same name) within nunjucks. You can override
-	 * gov.uk core filters by creating filter methods of the same name.
-	 * @type {Object}
-	 */
-	var filters = {}
+  /**
+   * Instantiate object used to store the methods registered as a
+   * 'filter' (of the same name) within nunjucks. You can override
+   * gov.uk core filters by creating filter methods of the same name.
+   * @type {Object}
+   */
+  var filters = {}
 
-	const numberToMonthString = (input) => {
-		const months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December',
-		]
-		return months[Number(input)]
-	}
+  const numberToMonthString = (input) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+    return months[Number(input)]
+  }
 
-	filters.month = (number) => numberToMonthString(number - 1)
+  filters.month = (number) => numberToMonthString(number - 1)
 
-	filters.dateFromInputs = (_, day, month, year) => {
-		const outputDate = Date.parse(
-			`${day} ${filters.month(month)} ${year} 00:00:00 GMT`
-		)
-		return outputDate
-	}
+  filters.dateFromInputs = (_, day, month, year) => {
+    const outputDate = Date.parse(
+      `${day} ${filters.month(month)} ${year} 00:00:00 GMT`
+    )
+    return outputDate
+  }
 
-	filters.friendlyDate = (str) => {
-		if (!str) {
-			return '-'
-		}
-		const date = new Date(str)
-		return (
-			date.getDate() +
-			' ' +
-			numberToMonthString(date.getMonth()) +
-			' ' +
-			date.getFullYear()
-		)
-	}
+  filters.friendlyDate = (str) => {
+    if (!str) {
+      return '-'
+    }
+    const date = new Date(str)
+    return (
+      date.getDate() +
+      ' ' +
+      numberToMonthString(date.getMonth()) +
+      ' ' +
+      date.getFullYear()
+    )
+  }
 
-	filters.autoClaimDate = (_, caringDate, awardDate, decisionDate) => {
-		const today = new Date()
-		const threeMonthsBeforeToday = today.setMonth(today.getMonth() - 3)
-		const decisionIsWithin3Months =
-			new Date(decisionDate) > threeMonthsBeforeToday
-		const qbDate = decisionIsWithin3Months
-			? new Date(awardDate)
-			: new Date(awardDate) > threeMonthsBeforeToday
-			? new Date(awardDate)
-			: threeMonthsBeforeToday
-		const dates = [new Date(caringDate), new Date(qbDate)]
-		let hasInvalidDate = false
-		let latestDate = new Date()
-		for (const date of dates) {
-			if (!(date instanceof Date)) {
-				hasInvalidDate = true
-			}
-		}
-		if (!hasInvalidDate) {
-			latestDate = new Date(
-				Math.max.apply(
-					null,
-					dates.map((date) => {
-						return date.getTime()
-					})
-				)
-			)
-		}
-		if (latestDate.getDay() != 1) {
-			return latestDate.setDate(
-				latestDate.getDate() + ((1 + 7 - latestDate.getDay()) % 7)
-			)
-		}
-		return latestDate
-	}
+  filters.autoClaimDate = (_, caringDate, awardDate, decisionDate) => {
+    const today = new Date()
+    const threeMonthsBeforeToday = today.setMonth(today.getMonth() - 3)
+    const decisionIsWithin3Months =
+      new Date(decisionDate) > threeMonthsBeforeToday
+    const qbDate = decisionIsWithin3Months
+      ? new Date(awardDate)
+      : new Date(awardDate) > threeMonthsBeforeToday
+        ? new Date(awardDate)
+        : threeMonthsBeforeToday
+    const dates = [new Date(caringDate), new Date(qbDate)]
+    let hasInvalidDate = false
+    let latestDate = new Date()
+    for (const date of dates) {
+      if (!(date instanceof Date)) {
+        hasInvalidDate = true
+      }
+    }
+    if (!hasInvalidDate) {
+      latestDate = new Date(
+        Math.max.apply(
+          null,
+          dates.map((date) => {
+            return date.getTime()
+          })
+        )
+      )
+    }
+    if (latestDate.getDay() != 1) {
+      return latestDate.setDate(
+        latestDate.getDate() + ((1 + 7 - latestDate.getDay()) % 7)
+      )
+    }
+    return latestDate
+  }
 
-	filters.getDay = (dateString) => {
-		const date = new Date(dateString)
-		return date.getDate()
-	}
+  filters.getDay = (dateString) => {
+    const date = new Date(dateString)
+    return date.getDate()
+  }
 
-	filters.getMonth = (dateString) => {
-		const date = new Date(dateString)
-		return date.getMonth() + 1
-	}
+  filters.getMonth = (dateString) => {
+    const date = new Date(dateString)
+    return date.getMonth() + 1
+  }
 
-	filters.getYear = (dateString) => {
-		const date = new Date(dateString)
-		return date.getFullYear()
-	}
+  filters.getYear = (dateString) => {
+    const date = new Date(dateString)
+    return date.getFullYear()
+  }
 
-	filters.getClaimDate = (data) => {
-		const dayInput = data['claim-date--claim-date-day'] || 23
-		const monthInput = data['claim-date--claim-date-month'] || 10
-		const yearInput = data['claim-date--claim-date-year'] || 2020
-		return `${dayInput} ${filters.month(monthInput)} ${yearInput}`
-	}
+  filters.getClaimDate = (data) => {
+    const dayInput = data['claim-date--claim-date-day'] || 23
+    const monthInput = data['claim-date--claim-date-month'] || 10
+    const yearInput = data['claim-date--claim-date-year'] || 2020
+    return `${dayInput} ${filters.month(monthInput)} ${yearInput}`
+  }
 
-	filters.addressOptions = (addressOptionArray, currentSelection) => {
-		currentSelection = currentSelection ? currentSelection : ''
-		if (Array.isArray(addressOptionArray)) {
-			const processedAddressOptionArray = addressOptionArray.map(
-				(addressOption) => {
-					return {
-						text: addressOption.text,
-						value: addressOption.value,
-						selected: addressOption.value == currentSelection ? true : false,
-					}
-				}
-			)
-			return processedAddressOptionArray
-		} else {
-			return []
-		}
-	}
+  filters.addressOptions = (addressOptionArray, currentSelection) => {
+    currentSelection = currentSelection ? currentSelection : ''
+    if (Array.isArray(addressOptionArray)) {
+      const processedAddressOptionArray = addressOptionArray.map(
+        (addressOption) => {
+          return {
+            text: addressOption.text,
+            value: addressOption.value,
+            selected: addressOption.value == currentSelection ? true : false,
+          }
+        }
+      )
+      return processedAddressOptionArray
+    } else {
+      return []
+    }
+  }
 
-	filters.includes = (arrayOfStrings, testString) => {
-		if (Array.isArray(arrayOfStrings)) {
-			if (arrayOfStrings.indexOf(testString) != -1) {
-				return true
-			}
-		}
-		return false
-	}
+  filters.includes = (arrayOfStrings, testString) => {
+    if (Array.isArray(arrayOfStrings)) {
+      if (arrayOfStrings.indexOf(testString) != -1) {
+        return true
+      }
+    }
+    return false
+  }
 
-	filters.debug = (obj) => {
-		return JSON.stringify(obj)
-	}
+  filters.debug = (obj) => {
+    return JSON.stringify(obj)
+  }
 
-	filters.lowerCase = (str) => (str ? str.toLowerCase() : '')
+  filters.lowerCase = (str) => (str ? str.toLowerCase() : '')
 
-	filters.upperCase = (str) => (str ? str.toUpperCase() : '')
+  filters.upperCase = (str) => (str ? str.toUpperCase() : '')
 
-	filters.titleCase = (str) => {
-		if (str) {
-			return str.replace(/\w\S*/g, (txt) => {
-				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-			})
-		} else {
-			return ''
-		}
-	}
+  filters.titleCase = (str) => {
+    if (str) {
+      return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+      })
+    } else {
+      return ''
+    }
+  }
 
-	filters.frequencySuffix = (response) => {
-		switch (response) {
-			case 'Once a week':
-				return 'a week'
+  filters.frequencySuffix = (response) => {
+    switch (response) {
+      case 'Once a week':
+        return 'a week'
 
-			case 'Every 2 weeks':
-				return 'every 2 weeks'
+      case 'Every 2 weeks':
+        return 'every 2 weeks'
 
-			case 'Every 4 weeks':
-				return 'every 4 weeks'
+      case 'Every 4 weeks':
+        return 'every 4 weeks'
 
-			default:
-				return 'a month'
-		}
-	}
+      default:
+        return 'a month'
+    }
+  }
 
-	filters.sentenceCase = (str) => {
-		if (str) {
-			return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()
-		} else {
-			return ''
-		}
-	}
+  filters.sentenceCase = (str) => {
+    if (str) {
+      return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()
+    } else {
+      return ''
+    }
+  }
 
-	filters.default = (dataItem, fallbackString) => {
-		if (isNotThere(dataItem)) {
-			return fallbackString
-		}
-		return dataItem
-	}
+  filters.default = (dataItem, fallbackString) => {
+    if (isNotThere(dataItem)) {
+      return fallbackString
+    }
+    return dataItem
+  }
 
-	filters.dateAsNumeric = (dayInput, monthInput, yearInput) => {
-		let queryDate = Date.parse(
-			`${dayInput} ${filters.month(monthInput)} ${yearInput} 12:00:00 GMT`
-		)
-		queryDate = new Date(queryDate)
-		if (isValidDate(queryDate)) {
-			return queryDate.getTime()
-		} else {
-			return false
-		}
-	}
+  filters.dateAsNumeric = (dayInput, monthInput, yearInput) => {
+    let queryDate = Date.parse(
+      `${dayInput} ${filters.month(monthInput)} ${yearInput} 12:00:00 GMT`
+    )
+    queryDate = new Date(queryDate)
+    if (isValidDate(queryDate)) {
+      return queryDate.getTime()
+    } else {
+      return false
+    }
+  }
 
-	filters.isWithinThreeMonths = (nowInput, dayInput, monthInput, yearInput) => {
-		let nowDate = new Date(nowInput)
-		let threeMonthsAgo = nowDate.setMonth(nowDate.getMonth() - 3)
-		let queryDate = Date.parse(
-			`${dayInput} ${filters.month(monthInput)} ${yearInput} 12:00:00 GMT`
-		)
-		queryDate = new Date(queryDate)
-		if (isValidDate(nowDate) && isValidDate(queryDate)) {
-			return queryDate > threeMonthsAgo
-		} else {
-			return false
-		}
-	}
+  filters.isWithinThreeMonths = (nowInput, dayInput, monthInput, yearInput) => {
+    let nowDate = new Date(nowInput)
+    let threeMonthsAgo = nowDate.setMonth(nowDate.getMonth() - 3)
+    let queryDate = Date.parse(
+      `${dayInput} ${filters.month(monthInput)} ${yearInput} 12:00:00 GMT`
+    )
+    queryDate = new Date(queryDate)
+    if (isValidDate(nowDate) && isValidDate(queryDate)) {
+      return queryDate > threeMonthsAgo
+    } else {
+      return false
+    }
+  }
 
-	filters.redirect = (location) => {
-		return `<script>window.location.href = '${location}';</script>`
-	}
+  filters.redirect = (location) => {
+    return `<script>window.location.href = '${location}';</script>`
+  }
 
-	filters.uuid = (selectedUUID) => {
-		if (isNotThere(selectedUUID)) {
-			return uuid()
-		}
-		return selectedUUID
-	}
+  filters.uuid = (selectedUUID) => {
+    if (isNotThere(selectedUUID)) {
+      return uuid()
+    }
+    return selectedUUID
+  }
 
-	/* ------------------------------------------------------------------
+  filters.calculateRecommendedDateOfClaim = (data) => {
+    const dateFormat = 'd M yyyy' // e.g. 01 02 2022 or 5 11 2020
+    const dateDisplayFormat = 'd MMMM yyyy' // 5 January 2020
+
+    // Date started providing care
+    const dateCaringDay = data['providing-care-day']
+    const dateCaringMonth = data['providing-care-month']
+    const dateCaringYear = data['providing-care-year']
+    const dateStartedCaring = DateTime.fromFormat(`${dateCaringDay} ${dateCaringMonth} ${dateCaringYear}`, dateFormat)
+
+    // Qualifying benefit start date
+    const qbStartDay = data['qb-start-day']
+    const qbStartMonth = data['qb-start-month']
+    const qbStartYear = data['qb-start-year']
+    const qbStartCaring = DateTime.fromFormat(`${qbStartDay} ${qbStartMonth} ${qbStartYear}`, dateFormat)
+
+    // Was QB awarded in the last 3 months?
+    const qbAwardedLastThreeMonths = data['qb-awarded-last-three-months']
+
+    // Date three months ago
+    const threeMonthsAgo = DateTime.now().minus({ months: 3 })
+
+    // Make sure started providing care is valid
+    if (dateStartedCaring.invalid !== null) {
+      return 'INVALID DATE STARTED CARING PROVIDED'
+    }
+
+    // Is the date started caring before three months ago?
+    if (dateStartedCaring < threeMonthsAgo) {
+      if (qbStartCaring.invalid !== null) {
+        // No qb start date entered, ask user to enter date of claim
+        return 'INVALID QB START DATE PROVIDED'
+      }
+
+      // Is the qb start date before three months ago?
+      if (qbStartCaring < threeMonthsAgo) {
+        if (qbAwardedLastThreeMonths === 'No') {
+          if (qbStartCaring < dateStartedCaring) {
+            // Recommend date started caring
+            return dateStartedCaring.toFormat(dateDisplayFormat)
+          }
+          // Recommend date qb started
+          return qbStartCaring.toFormat(dateDisplayFormat)
+        }
+        // Recommend date exactly three months ago
+        return threeMonthsAgo.toFormat(dateDisplayFormat)
+      }
+      // Recommend the qb start date
+      return qbStartCaring.toFormat(dateDisplayFormat)
+    }
+    // Date started providing care is in the last three months
+    // TODO: Find out what to recommend
+    return dateStartedCaring.toFormat(dateDisplayFormat)
+  }
+
+  filters.dateThreeMonthsAgo = () => {
+    const date = DateTime.now().minus({ months: 3 })
+    return date.toFormat('d MMMM yyyy') // 10 January 2022
+  }
+
+  filters.dateStartedCaring = (data) => {
+    const dateFormat = 'd M yyyy' // e.g. 01 02 2022 or 5 11 2020
+    const dateDisplayFormat = 'd MMMM yyyy' // 5 January 2020
+
+    // Date started providing care
+    const dateCaringDay = data['providing-care-day']
+    const dateCaringMonth = data['providing-care-month']
+    const dateCaringYear = data['providing-care-year']
+    const dateStartedCaring = DateTime.fromFormat(`${dateCaringDay} ${dateCaringMonth} ${dateCaringYear}`, dateFormat)
+
+    if (dateStartedCaring.invalid !== null) {
+      // No qb start date entered, ask user to enter date of claim
+      return 'INVALID CLAIM START DATE PROVIDED'
+    }
+
+    return dateStartedCaring.toFormat(dateDisplayFormat)
+  }
+
+  /* ------------------------------------------------------------------
     add your methods to the filters obj below this comment block:
     @example:
 
@@ -261,8 +340,8 @@ module.exports = function (env) {
 
   ------------------------------------------------------------------ */
 
-	/* ------------------------------------------------------------------
+  /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
   ------------------------------------------------------------------ */
-	return filters
+  return filters
 }
--- app/routes.js
+++ app/routes.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const { DateTime } = require('luxon');
 
 const config = require('./config')
 
 // Route to allow radio options to route to a given next page
 
 const radioButtonRedirect = (req, res, next) => {
-	const obj = Object.keys(req.body).length ? req.body : req.query
-	for (const k in obj) {
-		const v = obj[k]
-		if (typeof v === 'string' || v instanceof String) {
-			if (v.includes('~')) {
-				const parts = v.split('~')
-				req.session.data[k] = parts[0]
-				const href = parts[1]
-				return res.redirect(href)
-			}
-		}
-	}
-	next()
+  const obj = Object.keys(req.body).length ? req.body : req.query
+  for (const k in obj) {
+    const v = obj[k]
+    if (typeof v === 'string' || v instanceof String) {
+      if (v.includes('~')) {
+        const parts = v.split('~')
+        req.session.data[k] = parts[0]
+        const href = parts[1]
+        return res.redirect(href)
+      }
+    }
+  }
+  next()
 }
 
 router.use(radioButtonRedirect)
@@ -26,37 +27,97 @@ router.use(radioButtonRedirect)
 // Change service name in header based on URL path
 
 router.get('/*', (req, _, next) => {
-	req.session.data.headerTitle = config.serviceName
-	next()
+  req.session.data.headerTitle = config.serviceName
+  next()
+})
+
+router.post('/apply/ACA-1472/qb-startdate', (req, res, next) => {
+  const { data } = req.session
+
+  const dateFormat = 'd M yyyy' // e.g. 01 02 2022 or 5 11 2020
+
+  const dateCaringDay = data['providing-care-day']
+  const dateCaringMonth = data['providing-care-month']
+  const dateCaringYear = data['providing-care-year']
+  const dateStartedCaring = DateTime.fromFormat(`${dateCaringDay} ${dateCaringMonth} ${dateCaringYear}`, dateFormat)
+
+  // Qualifying benefit start date
+  const qbStartDay = data['qb-start-day']
+  const qbStartMonth = data['qb-start-month']
+  const qbStartYear = data['qb-start-year']
+  const qbStartCaring = DateTime.fromFormat(`${qbStartDay} ${qbStartMonth} ${qbStartYear}`, dateFormat)
+
+  // Date three months ago
+  const threeMonthsAgo = DateTime.now().minus({ months: 3 })
+
+  // Is the date started caring before three months ago?
+  if (dateStartedCaring < threeMonthsAgo) {
+    // Has a qb start date been provided?
+    if (qbStartCaring.invalid !== null) {
+      // Manual entry page
+      return res.redirect('/apply/ACA-1472/claimdate-3')
+    }
+
+    // Did the qb start longer than three months ago?
+    if (qbStartCaring < threeMonthsAgo) {
+      // Send to page to ask if benefit was awarded in last 3 months
+      return res.redirect('/apply/ACA-1472/qb-3months')
+    }
+    // If qb started in last three months we can make confident recommendation
+    return res.redirect('/apply/ACA-1472/claimdate-1')
+  }
+
+  // Date started caring must be in last three months at this point
+  // Has a qb start date been provided?
+  if (qbStartCaring.invalid !== null) {
+    // We can make an uncertain recommendation due to care start being in the last three months
+    return res.redirect('/apply/ACA-1472/claimdate-2')
+  } else {
+    return res.redirect('/apply/ACA-1472/claimdate-1')
+  }
+})
+
+router.post('/apply/ACA-1472/qb-3months', (req, res, next) => {
+  const { data } = req.session
+
+  // Was QB awarded in the last 3 months?
+  const qbAwardedLastThreeMonths = data['qb-awarded-last-three-months']
+
+  if (qbAwardedLastThreeMonths === 'Not sure') {
+    // We can make an uncertain recommendation due to care start being in the last three months
+    return res.redirect('/apply/ACA-1472/claimdate-3')
+  } else {
+    return res.redirect('/apply/ACA-1472/claimdate-1')
+  }
 })
 
 router.get('/apply/*', (req, _, next) => {
-	req.session.data.headerTitle = config.applyServiceName
-	next()
+  req.session.data.headerTitle = config.applyServiceName
+  next()
 })
 
 router.get('/poc/*', (req, _, next) => {
-	req.session.data.headerTitle = config.applyServiceName
-	next()
+  req.session.data.headerTitle = config.applyServiceName
+  next()
 })
 
 router.get('/changes/*', (req, _, next) => {
-	req.session.data.headerTitle = config.changesServiceName
-	next()
+  req.session.data.headerTitle = config.changesServiceName
+  next()
 })
 
 // Reset all one-time values
 
 const resetBooleanUnlessOverwritten = (req, name) => {
-	req.session.data[name] = req.body[name] || req.query[name] ? true : false
+  req.session.data[name] = req.body[name] || req.query[name] ? true : false
 }
 
 router.all('*', (req, _, next) => {
-	resetBooleanUnlessOverwritten(req, 'error')
-	resetBooleanUnlessOverwritten(req, 'show-buttons')
-	resetBooleanUnlessOverwritten(req, 'fromCheck')
-	req.session.data.time = new Date().getTime()
-	next()
+  resetBooleanUnlessOverwritten(req, 'error')
+  resetBooleanUnlessOverwritten(req, 'show-buttons')
+  resetBooleanUnlessOverwritten(req, 'fromCheck')
+  req.session.data.time = new Date().getTime()
+  next()
 })
 
 module.exports = router
